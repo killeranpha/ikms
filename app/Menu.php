eb80fd26167f525f2be8967a1eadcd8564a6821d
= <=>
2018-08-21 19:33:46 +0700

    2018-08-21-HieuND

--- app/Menu.php
+++ app/Menu.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     protected $table = 'menu';
-    static function GetMenuByChaId($chaId,$level){ 
-        $danhSach = Menu::where('chaId','=',$chaId)->where('daXoa',0)->get();
+    static function GetMenuByChaId($chaId,$level,$id){ 
+        $danhSach = Menu::where('chaId','=',$chaId)->where('daXoa',0)->where('id','!=',$id)->get();
         $array = [];
         foreach($danhSach as $key=>$value){
             $value->level = $level;
             $array[] = $value;
-            $array = array_merge($array,Menu::GetMenuByChaId($value->id,$level + 1));
+            $array = array_merge($array,Menu::GetMenuByChaId($value->id,$level + 1,$id));
         }
         return $array;
     }
